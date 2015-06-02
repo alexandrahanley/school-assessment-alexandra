e6d0d857dfa036f50249fe0c78dfedf8fbe55c2d
Alexandra Hanley <alexandrahanley@gmail.com>
2015-06-02 10:08:16 -0600

    add full_name method to instructor and  student models

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,9 @@
 class Instructor < ActiveRecord::Base
   has_many :lectures
   has_many :students, through: :lectures
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+  
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student < ActiveRecord::Base
   has_many :lectures
   has_many :instructors, through: :lectures
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
